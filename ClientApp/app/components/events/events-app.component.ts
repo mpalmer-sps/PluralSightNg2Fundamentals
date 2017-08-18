3933e08e9529f46189e04b7c1b9429f0d5927a8e
Martin Palmer <martinp@quietpathtech.com>
2017-08-18 15:29:31 -0700

    SUPER basic branding and basic event component created and working.

---
+++ ClientApp/app/components/events/events-app.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'events-app',
+    template: '<h2>Hello World from the Event Component!</h2>'
+})
+
+export class EventsAppComponent {
+    constructor() {
+        
+    }
+}
