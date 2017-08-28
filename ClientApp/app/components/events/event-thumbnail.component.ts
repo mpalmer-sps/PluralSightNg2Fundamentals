0a2753feff87310ae3a13ebca367726b6eaf7035
Martin Palmer <martinp@quietpathtech.com>
2017-08-27 20:36:19 -0700

    *ngIf, *ngSwitch Case and dynamic styling applied.

--- ClientApp/app/components/events/event-thumbnail.component.ts
+++ ClientApp/app/components/events/event-thumbnail.component.ts
@@ -4,6 +4,8 @@
     selector: 'event-thumbnail',
     templateUrl: './event-thumbnail.component.html',
     styles: [`
+        .green { color: #003300 !important; }
+        .bold { font-weight: bold; }
         .thumbnail { min-height: 250px; }
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb; }
@@ -18,9 +20,26 @@ export class EventThumbnailComponent {
         this.eventClickFromChild.emit(this.event.name);
     }
 
+    getStartTimeClass() {
+        // one approach for assigning classes is to return an object like the example below
+        // const isEarlyStart = this.event && this.event.time === '8:00 am';
+        // return { green: isEarlyStart, bold: isEarlyStart };
+
+        // another option is to return a string or an empty string and assign the classes that way
+        if (this.event && this.event.time === '8:00 am')
+        {
+            return 'green bold'
+        }
+        return ''
+        
+        
+    }
+
     logFoo() {
         console.log('FOO from child');
     }
+
+    foo: string = "foobar";
 
     constructor() {
 
