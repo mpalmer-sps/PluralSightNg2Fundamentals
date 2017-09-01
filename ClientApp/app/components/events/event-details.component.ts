6528955d7e9916cdd06d6c01fb4ef1599ce8a40b
Martin Palmer <martinp@quietpathtech.com>
2017-09-01 13:19:41 -0700

    baic routing links added for each event and the events list page -- also added images for each event

--- ClientApp/app/components/events/event-details.component.ts
+++ ClientApp/app/components/events/event-details.component.ts
@@ -1,5 +1,6 @@
 ﻿import { Component } from '@angular/core';
 import { EventService } from "../shared/event.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     templateUrl: './event-details.component.html',
@@ -13,9 +14,12 @@ export class EventDetailComponent {
     // properties
     event:any;
 
-    constructor(private eventSrv: EventService) { }
+    constructor(private eventSrv: EventService, private route:ActivatedRoute) { }
 
+    // the '+' on the parameter casts this to a number
+    // the id parameter must match the route map that is set in the app..module
+    // in that case this is "{ path: 'events/:id', component: EventDetailComponent },"
     ngOnInit() {
-        this.event = this.eventSrv.getEvent(1);
+        this.event = this.eventSrv.getEvent(+this.route.snapshot.params['id']);
     }
 }
--- ClientApp/app/components/shared/event.service.ts
+++ ClientApp/app/components/shared/event.service.ts
@@ -21,7 +21,7 @@ const EVENTS = [
         date: '9/26/2036',
         time: '10:00 am',
         price: 599.99,
-        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        imageUrl: '/images/events/angularconnect-shield.png',
         location: {
             address: '1057 DT',
             city: 'London',
@@ -99,7 +99,7 @@ const EVENTS = [
         date: '4/15/2037',
         time: '9:00 am',
         price: 950.00,
-        imageUrl: '/app/assets/images/ng-nl.png',
+        imageUrl: '/images/events/ng-nl.png',
         onlineUrl: 'http://ng-nl.org/',
         sessions: [
             {
@@ -155,7 +155,7 @@ const EVENTS = [
         date: '5/4/2037',
         time: '9:00 am',
         price: 759.00,
-        imageUrl: '/app/assets/images/ng-conf.png',
+        imageUrl: '/images/events/ng-conf.png',
         location: {
             address: 'The Palatial America Hotel',
             city: 'Salt Lake City',
@@ -237,7 +237,7 @@ const EVENTS = [
         date: '6/10/2037',
         time: '8:00 am',
         price: 800.00,
-        imageUrl: '/app/assets/images/basic-shield.png',
+        imageUrl: '/images/events/basic-shield.png',
         location: {
             address: 'The UN Angular Center',
             city: 'New York',
@@ -286,7 +286,7 @@ const EVENTS = [
         date: '2/10/2037',
         time: '9:00 am',
         price: 400.00,
-        imageUrl: '/app/assets/images/ng-vegas.png',
+        imageUrl: '/images/events/ng-vegas.png',
         location: {
             address: 'The Excalibur',
             city: 'Las Vegas',
